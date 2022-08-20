ead272857312117b5b9042138da64c0570d4fd83
camilo micheletto <camilomichelettors@gmail.com>
2022-08-20 00:43:00 -0300

    feat: (style) added front page style and voice switch

--- main.js
+++ main.js
@@ -2,9 +2,10 @@ const felipePhrases = ['Juliana?', 'achei','Adriano?','seu cu','faz isso comigo 
 
 const wordContainer = document.querySelector(".container")
 const generateButton = document.querySelector("[data-id=generator]")
+const soundConfig = document.querySelector("[data-id=sound]")
 let phrase = []
 
-const capitalize = (string) => string.charAt(0).toUpperCase() + string.slice(1)
+const capitalize = (string) => string.toString().charAt(0).toUpperCase() + string.toString().slice(1)
 
 function debounce(func, timeout = 300){
   let timer
@@ -44,20 +45,46 @@ const formPhrase = () => {
     return container;
 }
 
-const smitter = () => {
+const createVoiceSinthetizer = () => {
     const synth = window.speechSynthesis;
     const sinthText = phrase.join(" ")
     synth.cancel()
     let utterance = new SpeechSynthesisUtterance(sinthText);
     synth.speak(utterance)
+}
 
+const checkAudioConfig = () => localStorage.getItem('sound_profile')
+
+const setAudioConfig = () => {
+    const audioConfig = checkAudioConfig()
+    if (audioConfig) {
+        const audioSwitch = audioConfig === "on" ? "off" : "on"
+        localStorage.setItem('sound_profile', audioSwitch)
+        soundConfig.innerHTML = audioSwitch === "on" ? "Desligar som" : "Ligar som"
+        return
+    }
+    
+    localStorage.setItem('sound_profile', "on")
+    soundConfig.innerHTML = "Desligar som"
 }
 
 const generatePhraseContent = debounce(() => {
     const newWordContainer = formPhrase()
-    smitter()
+    checkAudioConfig() === "on" && createVoiceSinthetizer()
     phrase = []
     wordContainer.replaceChildren(newWordContainer)
 })
 
 generateButton.addEventListener("click", generatePhraseContent)
+soundConfig.addEventListener("click", setAudioConfig)
+
+document.addEventListener('keyup', event => {
+    if (event.code === 'Space') {
+        generatePhraseContent()
+    }
+  })
+
+window.addEventListener('DOMContentLoaded', () => {
+    const audioConfig = checkAudioConfig()
+    soundConfig.innerHTML = audioConfig === "on" ? "Desligar som" : "Ligar som"
+})
